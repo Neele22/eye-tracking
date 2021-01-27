2ced615b2ddd7228adea7ca93d1af4e4970cb1af
Neele Dijkstra <neeledijkstra@hotmail.com>
2021-01-27 16:23:06 +0100

    created jspsych plugin from webgazer calibration

--- www/js/resize_canvas.js
+++ www/js/resize_canvas.js
@@ -3,6 +3,7 @@
 * clears the canvas & then resizes it (as plots have moved position, can't resize without clear)
 */
 function resize() {
+    console.log('resizing');
     var canvas = document.getElementById('plotting_canvas');
     var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
